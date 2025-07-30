98175b5b3d929446b8fa9645e9e43f5f8df8d590
Yanhua CHEN <CONNECT\ychen484@fpga04.nice.intra>
2025-07-30 15:36:30 +0800

    utils: Modify to run all automatically

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd software-prefetch-multicast
-# bash gem5-compilation.sh
-# bash benchmark-compilation.sh
+bash gem5-compilation.sh
+bash benchmark-compilation.sh
 bash run-experiment-all.sh
 bash plot-figure.sh
